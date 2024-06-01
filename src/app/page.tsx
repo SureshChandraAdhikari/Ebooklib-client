b1f72d83211a1f296128c671fe45c4451175a8a4
Suresh Adhikari <sureshadhikari2002@gmail.com>
2024-06-01 15:04:17 +0530

    add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-<h1>Welcome To my project</h1>
-      
-
-    </main>
+    <>
+    <Banner />
+    </>
   );
 }
